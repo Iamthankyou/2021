ef562f3bc2e2b796aba58637858bad2a202b787f
iamthankyou <quangduynb00@gmail.com>
2021-04-30 17:43:28 +0700

    Explore event hander

--- Vue/basics-03-events-starting-code/basics-03-events-starting-code/app.js
+++ Vue/basics-03-events-starting-code/basics-03-events-starting-code/app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      confirmName:''
     };
   },
   methods:{
@@ -16,6 +17,14 @@ const app = Vue.createApp({
 
     setName(event, lastName){
       this.name = event.target.value + ' ' + lastName;
+    },
+
+    submitForm(event){
+      alert('Submited');
+    },
+
+    confirmInput(){
+      this.confirmName = this.name;
     }
     
   }
