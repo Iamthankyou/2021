9a31a7179d4cfd157b9980fbc5ef2c355e7b2729
iamthankyou <quangduynb00@gmail.com>
2021-04-30 17:02:17 +0700

    Understand event binding

--- Vue/basics-01-starting-code/basics-01-starting-code/app.js
+++ Vue/basics-01-starting-code/basics-01-starting-code/app.js
@@ -1,8 +1,8 @@
 const app = Vue.createApp({
     data(){
         return {
-            courseGoalA: 'This is data of courseGoalA',
-            courseGoalB: 'This is data of courseGoalB',
+            courseGoalA: '<h2>This is data of courseGoalA</h2>',
+            courseGoalB: '<h2>This is data of courseGoalB</h2>',
             vueLink: 'https://vuejs.org'
         };
     },
