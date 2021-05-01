cd4bbf1b7a9f4e96d81c0a09f9920d9a2f6ada2e
iamthankyou <quangduynb00@gmail.com>
2021-05-02 04:12:11 +0700

    Introding component

--- Vue/cmp-intro-01-starting-setup/cmp-intro-01-starting-setup/app.js
+++ Vue/cmp-intro-01-starting-setup/cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data(){
         return{
-            detailAreVisible: false,
             friends:[
                 {
                     id: 'manuel',
@@ -21,6 +20,37 @@ const app = Vue.createApp({
     },
 
     methods: {
+        toggleDetails(  ){
+            this.detailAreVisible = !this.detailAreVisible;
+        }
+    }
+});
+
+app.component('friend-contact', {
+    template: `
+    <li v-for="friend in friends" :key="friend.id">
+        <h2>{{friend.name}}</h2>
+        <button @click = "toggleDetails">{{detailAreVisible ? 'Hide': 'Show'}} Details</button>
+        <ul v-if="detailAreVisible  ">
+        <li><strong>Phone:</strong> {{friend.phone}}</li>
+        <li><strong>Email:</strong> {{friend.email}}</li>
+    </li>
+    `,
+    data(){
+        return {
+            detailAreVisible: false,
+            friends:[
+                {
+                    id: 'manuel',
+                    name: 'Nguyễn Quỳnh Anh',
+                    phone: '0123456789',
+                    email: 'quanh@gmail.com'
+                }
+            ]
+        };
+    },
+
+    methods:{
         toggleDetails(){
             this.detailAreVisible = !this.detailAreVisible;
         }
