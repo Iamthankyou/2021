8d86157dddea203106916e74df5c0affae53b9b8
iamthankyou <quangduynb00@gmail.com>
2021-05-03 15:50:31 +0700

    Init: The Learning Resources App

---
+++ Vue/prj-cmp-01-starting-setup/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul>
+        
+    </ul>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            storeResources:[
+                {
+                    id:'official-guide',
+                    title:'Official Guide',
+                    description:'The official Vue.js documentaion.',
+                    link:'https://vuejs.org'
+                },
+                {
+                    id:'google',
+                    title:'Google',
+                    description:'The official Vue.js documentaion.',
+                    link:'https://vuejs.org'
+                }
+            ]
+        }
+    }
+}
+</script>
