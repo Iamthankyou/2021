faa9fb98c8edb4c4d3123c21a9cc70337d585c29
iamthankyou <quangduynb00@gmail.com>
2021-05-03 10:43:58 +0700

    Scope slot

--- Vue/cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/main.js
+++ Vue/cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/main.js
@@ -5,6 +5,7 @@ import BaseBadge from './components/BaseBadge.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
 import BaseCard from './components/BaseCard.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 const app = createApp(App);
 
@@ -12,5 +13,6 @@ app.component('base-badge', BaseBadge);
 app.component('badge-list', BadgeList);
 app.component('user-info', UserInfo);
 app.component('base-card', BaseCard);
+app.component('course-goals', CourseGoals);
 
 app.mount('#app');
