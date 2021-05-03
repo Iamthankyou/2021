202b1d9c2562499a59b9864eaadbaccd28d48260
iamthankyou <quangduynb00@gmail.com>
2021-05-03 16:10:20 +0700

    First components & props

--- Vue/prj-cmp-01-starting-setup/prj-cmp-01-starting-setup/src/App.vue
+++ Vue/prj-cmp-01-starting-setup/prj-cmp-01-starting-setup/src/App.vue
@@ -1,11 +1,23 @@
 <template>
     <ul>
-        
+        <learning-resource
+            v-for="item in storeResources"
+            :key="item.id"
+            :id="item.id"
+            :title="item.title"
+            :description="item.description"
+            :link="item.link"
+        ></learning-resource>
     </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resource/LearningResource';
 export default {
+    components:{
+        LearningResource
+    },
+
     data(){
         return {
             storeResources:[
