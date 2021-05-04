011bb584c6cf22366b5990da1f1b64a49dd27025
iamthankyou <quangduynb00@gmail.com>
2021-05-04 16:27:22 +0700

    Redirecting & catch all router

--- Vue/routing-01-starting-setup/routing-01-starting-setup/src/main.js
+++ Vue/routing-01-starting-setup/routing-01-starting-setup/src/main.js
@@ -5,13 +5,16 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/teams',component:TeamsList}, // our-domain.com/team => TeamsList
+        // {path: '/', alia},
+        {path: '/teams',component:TeamsList, alias:"/"}, // our-domain.com/team => TeamsList
         {path: '/users',component:UsersList},
-        {path: '/teams/:teamId', component:TeamMembers, props: true}// our-domain.com/team => TeamsList
+        {path: '/teams/:teamId', component:TeamMembers, props: true},// our-domain.com/team => TeamsList
+        {path: '/:notFound(.*)', component:NotFound}
     ],
     linkActiveClass: 'active'
 });
