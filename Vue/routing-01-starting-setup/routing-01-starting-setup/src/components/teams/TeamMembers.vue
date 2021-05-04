0c8f0c24d6078e0e714ac28c4af4eb560bd7272e
iamthankyou <quangduynb00@gmail.com>
2021-05-04 16:04:40 +0700

    Update param data with watcher

--- Vue/routing-01-starting-setup/routing-01-starting-setup/src/components/teams/TeamMembers.vue
+++ Vue/routing-01-starting-setup/routing-01-starting-setup/src/components/teams/TeamMembers.vue
@@ -9,6 +9,7 @@
         :role="member.role"
       ></user-item>
     </ul>
+    <router-link to="/teams/t2">Go to team 2</router-link>
   </section>
 </template>
 
@@ -16,18 +17,47 @@
 import UserItem from '../users/UserItem.vue';
 
 export default {
+  inject: ['users','teams'],
   components: {
     UserItem
   },
   data() {
     return {
-      teamName: 'Test',
-      members: [
-        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
-        { id: 'u2', fullName: 'Max Schwarz', role: 'Engineer' },
-      ],
-    };
+      teamName: '',
+      members:[]
+    }
   },
+
+  created(){
+    // this.$router.path //teams/t1
+    this.loadTeamMember(this.$route);
+  },
+  methods:{
+    loadTeamMember(route){
+      console.log(route.params.teamId);
+      const teamId = route.params.teamId;
+      const selectedTeam = this.teams.find(team=>team.id === teamId);
+      console.log(teamId);
+      const members = selectedTeam.members;
+      const selectedMembers = [];
+      
+      for (const member of members){
+        const selectedUser = this.users.find(user=>user.id === member);
+        selectedMembers.push(selectedUser);
+      }
+
+      this.members = selectedMembers;
+      this.teamName = selectedTeam.name;  
+    }
+  },
+
+  watch: {
+    $route(newRoute){
+      if (newRoute.params.teamId) {
+        this.loadTeamMember(newRoute);
+      }
+    }
+  }
 };
 </script>
 
